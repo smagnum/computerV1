27bb163bd6341579f3bbc1a8dbbf45807447d6aa
mac <souhail123razi'gmail.comk>
2021-03-13 15:15:38 +0100

    13032021

--- read.py
+++ read.py
@@ -1,18 +1,32 @@
 import sys
 
+def duplicates(lst, item):
+    return [i for i, x in enumerate(lst) if x == item]
 
 if len(sys.argv) != 2 :
     print ('Invalid arguments')
     sys.exit()
 else :
+    # GET first argument
     params = str(sys.argv[1])
    
+    # CHANGE - with +-
     params = params.replace('-', '+-')
+
+    # GET the first and second params
     params = params.split('=')
 
     param1 = params[0]
     param2 = params[1]
 
+    # CHECK negaitve
+    if param1[0] == '+' and param1[1] == '-' :
+        param1 = param1[1:]
+
+    if param2[0] == '+' and param2[1] == '-' :
+        param2 = param2[1:]
+
+    # GET the vars and puiss
     param1 = param1.split('+')
     param2 = param2.split('+')
 
@@ -54,13 +68,105 @@ else :
             var2.append(x)
             puiss2.append('X^0')
         j += 1
-    
+
     print (var1)
     print (puiss1)
-    print ('=================')
     print (var2)
     print (puiss2)
-    print ('=================')
+    
+    # CHECK DUPLICATED
+    print ('MAINTENANCE')
+    dup = duplicates(puiss1, puiss1[0])
+    print (dup)
+
+    i = 1
+    while i < len(dup):
+        print (var1)
+        var1[dup[0]] = float(var1[dup[0]]) + float(var1[dup[j]])
+        i += 1
+    
+    i = 1 
+    while i < len(dup):
+        # del(puiss1[dup[i]])
+        print ('HAAAAAHUWA')
+        print (var1[dup[i]])
+        i += 1
+    print ('var1')
+    print (var1)
+
+    print ('MAINTENANCE')
+
+    # while i < len(puiss1):
+        
+    #     print (dup)
+    #     i += 1
+    # CHECK DUPLICATED
+    # i = 0
+    # while i < len(puiss1):
+    #     dup = duplicates(puiss1, puiss1[i])
+
+    #     if len(dup) > 1:
+    #         j = 1
+    #         while j < len(dup) :
+    #             print (j)
+    #             print (dup)
+    #             # print (len(dup))
+    #             print (dup[j])
+    #             print (var1)
+
+    #             try :
+    #                 var1[dup[0]] = float(var1[dup[0]]) + float(var1[dup[j]])
+    #                 del(puiss1[dup[j]])
+    #                 del(var1[dup[j]]) 
+    #             except :
+    #                 print ('hania2')
+                
+    #             try :
+    #                 del(dup[j+1])
+    #             except:
+    #                 print ('hania')
+           
+    #             #dup[j+1] = int(dup[j+1]) - 1
+                
+    #             j+=1
+    #         # print (dup)
+    #     i += 1
+
+
+
+    i = 0
+    while i < len(puiss2):
+        dup = duplicates(puiss2, puiss2[i])
+        if len(dup) > 1:
+            j = 1
+            while j < len(dup) :
+                # print (dup[j])
+                print (i)
+                
+                var2[dup[0]] = float(var2[dup[0]]) + float(var2[dup[j]])
+                del(puiss2[dup[j]])
+                del(var2[dup[j]])
+                j+=1
+            # print (dup)
+        i += 1
+
+    # print (var1)
+    # print (puiss1)
+    # print (var2)
+    # print (puiss2)
+    # print (var1)
+    # print (var2)
+    # print (puiss1)
+    # print (puiss2)
+  
+
+    
+    # print (var1)
+    # print (puiss1)
+    # print ('=================')
+    # print (var2)
+    # print (puiss2)
+    # print ('=================')
 
     # len_var = len1 - len2
     # if len_var > 0 :
@@ -77,17 +183,19 @@ else :
     #     k += 1 
 
 
-
+    # CHECK the same puiss and do addition
+    len1 = len(puiss1)
+    len2 = len(puiss2)
     exist = []
     k = 0
     while k < len1:
         u = 0
         while u < len2:
             if puiss1[k] == puiss2[u] :
-                print ('puiss1')
-                print (puiss1[k])
-                print ('puiss2')
-                print (puiss2[u])
+                # print ('puiss1')
+                # print (puiss1[k])
+                # print ('puiss2')
+                # print (puiss2[u])
                 var1[k] = float(var1[k]) - float(var2[u])
                 exist.append(puiss2[u])
 
@@ -96,57 +204,82 @@ else :
         k += 1
 
 
-# print ('////////@@@VAR1@@@@//////')
-# print (var1)
+    # print ('////////@@@VAR1@@@@//////')
+    # print (var1)
 
-print ('////// @@@@@exist@@@@ ////////')
-print (exist)
-
-
-print ('////// @@@@@DIFF@@@@ ////////')
-# XOR operator: 
-diff = set(exist) ^ set(puiss2)
-
-print (diff)
-
-for di in diff :
-    i = 0 
-    while i < len(puiss2):
-        if di == puiss2[i] :
-            print (di)
-            # print (i)
-            puiss1.append(di)
-            var1.append(int(var2[i]) * -1)
-        i += 1
-    # if di in puiss2 :
-    #     print (di)
-    #     i = 0 
-    #     while i < di :
-    #         if 
+    # print ('////// @@@@@exist@@@@ ////////')
+    # print (exist)
 
 
-print ('////////@@@FINAL_VAR@@@@//////')
-print (var1)
-
-print ('////////@@@FINAL_PUISS@@@@//////')
-print (puiss1)
+    # print ('////// @@@@@DIFF@@@@ ////////')
 
 
+    # XOR operator: 
+    diff = set(exist) ^ set(puiss2)
 
-len_res = len(var1)
-print (len_res)
 
-t = 0
-while t < len_res :
-    var1[t] = str(var1[t]) + '*' + puiss1[t]
-    t += 1
+    # print (diff)
 
-print ('#########RESULT#########')
-print (var1)
+    for di in diff :
+        i = 0 
+        while i < len(puiss2):
+            if di == puiss2[i] :
+                # print (di)
+                # print (i)
+                puiss1.append(di)
+                var1.append(int(var2[i]) * -1)
+            i += 1
+        # if di in puiss2 :
+        #     print (di)
+        #     i = 0 
+        #     while i < di :
+        #         if 
 
-new = sorted(var1, key=lambda x: int(x[-1]))
 
-print ('///new///')
-print (new)
+    # print ('////////@@@FINAL_VAR@@@@//////')
+    # print (var1)
+
+    # print ('////////@@@FINAL_PUISS@@@@//////')
+    # print (puiss1)
+
+    print (var1)
+    print (puiss1)
+
+
+
+
+
+
+    len_res = len(var1)
+    # print (len_res)
+
+                # CHECK DUPLICATED
+    # i = 0
+    # while i < len_res:
+    #     dup = duplicates(puiss1, puiss1[i])
+    #     print (dup)
+    #     if len(dup) > 1 :
+    #         j = 0
+    #         while j < len(dup) :
+    #             var1[dup[0]] = int(var1[dup[0]]) + int(var1[dup[j]])
+    #             j += 1
+    #         print (dup)
+    #         print ('////// VAR1 ///////')
+    #         print (var1)
+    #     i += 1
+
+
+    t = 0
+    while t < len_res :
+        var1[t] = str(var1[t]) + '*' + puiss1[t]
+        t += 1
+
+    # print ('#########RESULT#########')
+    # print (var1)
+
+    new = sorted(var1, key=lambda x: int(x[-1]))
+
+    # print ('///new///')
+    print (new)
 
 
